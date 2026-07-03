75eac25821a4039b2552b78546191635bc00cf6e
DevoraHQ <tijesuniemmanuella@icloud.com>
2026-07-03 13:15:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -179,3 +179,16 @@ toggleSearch.addEventListener("click", () => {
     }
 
 });
+const menuToggle = document.getElementById("menuToggle");
+const sideMenu = document.getElementById("sideMenu");
+
+menuToggle.addEventListener("click", () => {
+    sideMenu.classList.add("active");
+});
+document.addEventListener("click", (e) => {
+
+    if (!sideMenu.contains(e.target) && !menuToggle.contains(e.target)) {
+        sideMenu.classList.remove("active");
+    }
+
+});
